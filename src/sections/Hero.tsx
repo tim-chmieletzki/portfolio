9b23ab79f29eaec09ab0761c9b65a0101688f9d7
tim-chmieletzki <timchmieletzki@gmail.com>
2025-04-06 01:17:50 +0200

    Added content to hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,37 @@
+"use client";
+import { Space_Mono } from "next/font/google";
+
+const space_mono = Space_Mono({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
 export const Hero = () => {
-  return <section className="mt-12 h-[2000px]">Hero</section>;
+  const infos = ["TIM", "21", "STUDENT", "BHT"];
+  const icons = [];
+
+  return (
+    <section className="border border-accentPrim mx-4">
+      <div className={`${space_mono.className} h-[320px] leading-5`}>
+        <div className="flex flex-col items-start">
+          {infos.map((item, index) => (
+            <div
+              key={index}
+              className="py-2 px-5 border border-textSec rounded-xl"
+            >
+              {item}
+            </div>
+          ))}
+          {icons.map((Icon, index) => (
+            <div
+              key={index}
+              className="p-2 text-background bg-textPrim rounded-full flex items-center justify-center"
+            >
+              <Icon className="w-5 h-5" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
