77b0842b1d5bd0b25840aa04c1e37fbce1711236
tim-chmieletzki <timchmieletzki@gmail.com>
2025-04-06 17:08:18 +0200

    Updated header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,10 +2,10 @@ export const Header = () => {
   return (
     <header className="flex flex-col mx-4 mt-4 font-inter text-sm leading-4 tracking-tighter">
       <div className="flex justify-between items-center">
-        <span className="text-textSec">TIM CHMIELETZKI</span>
+        <span className="text-textSec">TIMCHMIELETZKI@GMAIL.COM</span>
         <div className="bg-accentPrim rounded-full w-5 h-5" />
       </div>
-      <div className="flex flex-col justify-start items-start gap-[2px] mt-4">
+      <div className="flex flex-col justify-start items-start mt-4">
         <h1>WIRTSCHAFTSINFORMATIKSTUDENT</h1>
         <div className="flex justify-start items-center">
           <span>MV, DEUTSCHLAND</span>
