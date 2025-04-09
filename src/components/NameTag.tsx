8b0ea04ca45da08cfe75889965db411486a52fea
tim-chmieletzki <timchmieletzki@gmail.com>
2025-04-09 12:17:15 +0200

    Updated hero

--- src/components/NameTag.tsx
+++
@@ -1,7 +0,0 @@
-export const NameTag = () => {
-  return (
-    <h1 className="block w-full font-medium text-5xl text-center tracking-tighter whitespace-nowrap">
-      TIM CHMIELETZKI
-    </h1>
-  );
-};
