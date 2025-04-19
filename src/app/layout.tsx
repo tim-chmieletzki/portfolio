68e85d6adca5d7d3e555bdeb84de1b9af3e80d97
tim-chmieletzki <timchmieletzki@gmail.com>
2025-04-19 22:33:02 +0200

    Added big-Shoulder-Display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,16 @@ import "./global.css";
 import type { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 import { Manrope } from "next/font/google";
+import { Big_Shoulders_Display } from "next/font/google";
 
 const manrope = Manrope({
   subsets: ["latin"],
   variable: "--font-manrope",
+});
+
+const bigShouldersDisplay = Big_Shoulders_Display({
+  subsets: ["latin"],
+  variable: "--font-bigShouldersDisplay",
 });
 
 export const metadata: Metadata = {
@@ -22,7 +28,7 @@ export default function RootLayout({
     <html lang="de">
       <body
         className={twMerge(
-          manrope.variable,
+          bigShouldersDisplay.variable,
           "bg-background text-textPrim antialiased"
         )}
       >
