7d611470a514f3d6a619ef5d7f98b1361e0be028
tim-chmieletzki <timchmieletzki@gmail.com>
2025-08-16 22:26:29 +0200

    Updated Hero

---
+++ src/sections/start/Hero.tsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section>
+      <div>
+        <a href="#" className="mono weight-500">
+          TIM CHMIELETZKI
+        </a>
+        <nav>
+          <a href=""></a>
+        </nav>
+      </div>
+    </section>
+  );
+}
